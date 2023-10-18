dad7596eea4cbb1331dc728974836779e525bb30
Alexander Vogel <amv@vogel-consulting.de>
2023-10-18 15:49:54 +0200

    Some Cleanups

--- src/main/java/advent/of/code/twofifteen/Day4.java
+++ src/main/java/advent/of/code/twofifteen/Day4.java
@@ -9,7 +9,7 @@ public class Day4 {
     public int lowestForMD5() throws NoSuchAlgorithmException {
         var secret = "ckczppom";
         var result = 0;
-
+ 
         while (true) {
             var currentValue = secret + result;
             var hashString = DigestUtils.md5Hex(currentValue);
