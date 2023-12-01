2e258efcfc3d8a11a79a999d7dff72ff52fb0d90
Alexander Vogel <amv@vogel-consulting.de>
2023-12-01 13:26:30 +0100

    cleanup

--- src/main/java/advent/of/code/twentytwo/Day1.java
+++ src/main/java/advent/of/code/twentytwo/Day1.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -32,7 +31,6 @@ public class Day1 {
 			});
 
 			listOfPackaged.sort(Collections.reverseOrder());
-
 
 			System.out.println(listOfPackaged);
 
